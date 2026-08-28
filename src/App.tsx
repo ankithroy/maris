1d686e67da087b82e361375c939953ae3de74556
MARIS <maris@sih.dev>
2026-08-28 21:39:02 +0530

    fix: add router basename for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { Settings } from './pages/Settings';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
